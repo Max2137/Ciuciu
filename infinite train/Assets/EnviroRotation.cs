ecae25c2e42c31a9a1ec8780429b3d8d75529cba
XDDDman <114019783+XDDDman@users.noreply.github.com>
2024-10-06 14:25:28 +0200

    Better woodlog

--- infinite train/Assets/EnviroRotation.cs
+++ infinite train/Assets/EnviroRotation.cs
@@ -15,6 +15,9 @@ public class EnviroRotation : MonoBehaviour
     // Flaga okreœlaj¹ca kierunek rotacji: true = z A do B, false = z B do A
     private bool isRotatingToEnd = true;
 
+    // Si³a ró¿nicy miêdzy szybkim a wolnym tempem (1 = standardowe, wiêksza wartoœæ = wiêksza ró¿nica)
+    public float easeStrength = 1f;
+
     // Ustawienie pocz¹tkowej rotacji
     void Start()
     {
@@ -28,20 +31,23 @@ public class EnviroRotation : MonoBehaviour
         // Zwiêkszaj postêp animacji
         rotationProgress += Time.deltaTime / rotationDuration;
 
+        // Obliczenie progresji z u¿yciem funkcji sinusoidalnej oraz si³y ró¿nicy tempa (easeStrength)
+        float easedProgress = Mathf.Pow(Mathf.Sin(rotationProgress * Mathf.PI * 0.5f), easeStrength);
+
         // SprawdŸ kierunek rotacji i interpoluj odpowiednio
         if (isRotatingToEnd)
         {
             // Rotacja z A do B
             Quaternion startQuat = Quaternion.Euler(startRotation);
             Quaternion endQuat = Quaternion.Euler(endRotation);
-            transform.rotation = Quaternion.Lerp(startQuat, endQuat, rotationProgress);
+            transform.rotation = Quaternion.Lerp(startQuat, endQuat, easedProgress);
         }
         else
         {
             // Rotacja z B do A
             Quaternion startQuat = Quaternion.Euler(endRotation);
             Quaternion endQuat = Quaternion.Euler(startRotation);
-            transform.rotation = Quaternion.Lerp(startQuat, endQuat, rotationProgress);
+            transform.rotation = Quaternion.Lerp(startQuat, endQuat, easedProgress);
         }
 
         // Po zakoñczeniu rotacji zmieñ kierunek i zresetuj licznik czasu
